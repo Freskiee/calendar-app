510f4cfbd336e36cbe26698ea8afdee319627a6a
Ariel Sanchez <freska_96@hotmail.com>
2024-07-31 08:23:30 -0600

    feat: creacion del componente CalendarEvent, para llamar el evento y desestructurar el titulo y el usuario

--- src/calendar/pages/CalendarPage.jsx
+++ src/calendar/pages/CalendarPage.jsx
@@ -1,8 +1,9 @@
+/* eslint-disable no-unused-vars */
 import { Calendar } from 'react-big-calendar';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 
 import { addHours } from 'date-fns';
-import { Navbar } from "../";
+import { CalendarEvent, Navbar } from "../";
 
 import { localizer, getMessagesEs } from '../../helpers';
 
@@ -22,7 +23,6 @@ const events = [{
 export const CalendarPage = () => {
 
     const eventStyleGetter = (event, start, end, isSelected) => {
-        console.log({event, start, end, isSelected});
 
         const style = {
             backgroundColor: '#347cf7',
@@ -49,6 +49,9 @@ export const CalendarPage = () => {
                 style={{ height: 'calc(100vh - 80px)' }}
                 messages={getMessagesEs()}
                 eventPropGetter={eventStyleGetter}
+                components={{
+                    event: CalendarEvent
+                }}
             />
 
         </>
