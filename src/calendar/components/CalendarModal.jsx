60e7f9c5b58bddc67987e0321d2b615c46dc2288
Ariel Sanchez <freska_96@hotmail.com>
2024-07-31 12:09:13 -0600

    feat: creando el componente onSubmit, para validar valores y marcar errores

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -1,6 +1,6 @@
 
 import { useState } from 'react';
-import { addHours } from 'date-fns';
+import { addHours, differenceInSeconds } from 'date-fns';
 
 import Modal from 'react-modal';
 import DatePicker, { registerLocale } from "react-datepicker";
@@ -51,6 +51,26 @@ export const CalendarModal = () => {
         setIsOpen(false);
     };
 
+    const onSubmit = (event) => {
+        event.preventDefault();
+
+        const difference = differenceInSeconds(formValues.end, formValues.start);
+
+        if (isNaN(difference) || difference <= 0) {
+            console.log('Error en fechas');
+            return;
+        }
+
+        if (formValues.title.length <= 0) return;
+
+        console.log(formValues);
+
+        // TODO:
+        // Remover errores en la pantalla
+        // Cerrar el modal
+
+    };
+
     return (
         <Modal
             isOpen={isOpen}
@@ -62,7 +82,7 @@ export const CalendarModal = () => {
         >
             <h1> Nuevo evento </h1>
             <hr />
-            <form className="container">
+            <form className="container" onSubmit={onSubmit}>
 
                 <div className="form-group mb-2">
                     <label>Fecha y hora inicio</label>
