65a14869ac06c12c6bdebf0b7c5d42ab92d8843d
Ariel Sanchez <freska_96@hotmail.com>
2024-08-08 12:01:50 -0600

    feat: añadiendo un nuevo evento mediante el reducer de onAddNewEvent

--- src/calendar/components/CalendarModal.jsx
+++ src/calendar/components/CalendarModal.jsx
@@ -31,7 +31,7 @@ Modal.setAppElement('#root');
 export const CalendarModal = () => {
 
     const { isDateModalOpen, closeDateModal } = useUiStore();
-    const { activeEvent } = useCalendarStore()
+    const { activeEvent, startSavingEvent } = useCalendarStore()
 
     const [formSubmitted, setFormSubmitted] = useState(false);
 
@@ -77,7 +77,7 @@ export const CalendarModal = () => {
         closeDateModal();
     };
 
-    const onSubmit = (event) => {
+    const onSubmit = async (event) => {
         event.preventDefault();
         setFormSubmitted(true);
 
@@ -95,6 +95,9 @@ export const CalendarModal = () => {
         // TODO:
         // Remover errores en la pantalla
         // Cerrar el modal
+        await startSavingEvent(formValues);
+        closeDateModal();
+        setFormSubmitted(false);
 
     };
 
