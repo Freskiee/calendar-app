d15c6c5da76a8eafe855ec14c690c1a1925f2ef1
Ariel Sanchez <freska_96@hotmail.com>
2024-08-31 04:27:50 -0600

    feat: creando funcion startDeletingEvent para eliminar la nota seleccionada, escondiendo el boton de delete mediante la funcion hasEventSelected

--- src/calendar/components/FabDelete.jsx
+++ src/calendar/components/FabDelete.jsx
@@ -3,16 +3,19 @@ import { useCalendarStore } from "../../hooks";
 
 export const FabDelete = () => {
 
-    const {  } = useCalendarStore();
+    const { startDeletingEvent, hasEventSelected } = useCalendarStore();
 
-    const handleClickNew = () => {
-
+    const handleDelete = () => {
+        startDeletingEvent();
     };
 
     return (
         <button
             className="btn btn-danger fab-danger"
-            onClick={handleClickNew}
+            onClick={handleDelete}
+            style={{
+                display: hasEventSelected ? '' : 'none'
+            }}
         >
             <i className="fas fa-trash-alt"></i>
         </button>
--- src/store/calendar/calendarSlice.js
+++ src/store/calendar/calendarSlice.js
@@ -31,18 +31,21 @@ export const calendarSlice = createSlice({
             state.events.push(payload);
             state.activeEvent = null;
         },
-        onUpdateEvent: (state, {payload}) => {
+        onUpdateEvent: (state, { payload }) => {
             state.events = state.events.map(event => {
-                if(event._id === payload._id) {
+                if (event._id === payload._id) {
                     return payload;
                 }
                 return event;
             });
         },
-        onDeleteEvent: () => {
-            
+        onDeleteEvent: (state) => {
+            if (state.activeEvent) {
+                state.events = state.events.filter(event => event._id !== state.activeEvent._id);
+                state.activeEvent = null;
+            }
         }
     },
 });
 // Action creators are generated for each case reducer function
-export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent } = calendarSlice.actions;
+export const { onSetActiveEvent, onAddNewEvent, onUpdateEvent, onDeleteEvent } = calendarSlice.actions;
